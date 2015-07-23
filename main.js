37829a19d800a70850a8de61caa96bdf5524aa07
Simon lucas <simonlucas120@gmail.com>
2015-07-22 22:00:05 -0600

    moving target works
    yeah!

---
+++ main.js
@@ -0,0 +1,23 @@
+"use strict";
+
+var target = document.getElementById("target");
+var 	targetPosition=0;
+var direction=1;
+
+
+function moveTarget(){
+	if (targetPosition > window.innerWidth-500) {
+		direction=-1;
+	} 
+
+if (targetPosition < 0) {
+		direction=1;
+}
+
+	targetPosition += 10*direction;
+	target.style.left= targetPosition + "px";
+
+	
+}
+
+setInterval(moveTarget,1000/60);
